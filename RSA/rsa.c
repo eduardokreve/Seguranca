9d089edbcefdb50fa7dc4700d104ee0ffc4f025c
eduardokreve <eduardokreve283@gmail.com>
2018-11-04 01:38:57 -0200

    Teste tabs

--- RSA/rsa.c
+++ RSA/rsa.c
@@ -4,13 +4,13 @@
 #include "funcoes.h"
 
 void clear() { //https://stackoverflow.com/questions/2347770/how-do-you-clear-the-console-screen-in-c
-    #if defined(__linux__) || defined(__unix__) || defined(__APPLE__)
-        system("clear");
-    #endif
+	#if defined(__linux__) || defined(__unix__) || defined(__APPLE__)
+		system("clear");
+	#endif
 
-    #if defined(_WIN32) || defined(_WIN64)
-        system("cls");
-    #endif
+	#if defined(_WIN32) || defined(_WIN64)
+		system("cls");
+	#endif
 }
 
 int primo(int X) {
@@ -20,40 +20,40 @@ int primo(int X) {
 		if(X % i == 0) cont++;
 	}
 
-    if(cont == 2) return X; //é primo
+	if(cont == 2) return X; //é primo
 
 	else if(cont > 2 || cont < 2){
 		clear();
-        printf("Numero digitado nao eh impar\nDigite novamente: ");
-        scanf("%d", &X);
-        primo(X);
+		printf("Numero digitado nao eh impar\nDigite novamente: ");
+		scanf("%d", &X);
+		primo(X);
 	}
 
-    return X;
+	return X;
 }
 
 /*int calcE(int n) {
-    int i
+	int i
 
 }
 */
 int main() {
-    int n = 0, p = 0, q = 0, totiente, e;
+	int n = 0, p = 0, q = 0, totiente, e;
 
-    printf("p: ");
-    scanf("%d", &p);
-    p = primo(p);
+	printf("p: ");
+	scanf("%d", &p);
+	p = primo(p);
 
-    printf("q: ");
-    scanf("%d", &q);
-    q = primo(q);
+	printf("q: ");
+	scanf("%d", &q);
+	q = primo(q);
 
-    n = p * q;
-    printf("%d = %d * %d\n", n, p, q);
+	n = p * q;
+	printf("%d = %d * %d\n", n, p, q);
 
-    //calcula chave publica
-    totiente = (p-1)*(q-1);
-    calcE(totiente);
+	//calcula chave publica
+	totiente = (p-1)*(q-1);
+	calcE(totiente);
 
-    return 0;
+	return 0;
 }
